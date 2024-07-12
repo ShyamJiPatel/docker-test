8fa53752ab7d600d1b2c941108f9beae5c336441
shyamji <patelshyamji4@gmail.com>
2024-07-12 17:05:39 +0530

    converted war to jar application

--- src/main/java/com/docker/test/TestApplication.java
+++ src/main/java/com/docker/test/TestApplication.java
@@ -2,14 +2,13 @@ package com.docker.test;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.boot.web.servlet.support.SpringBootServletInitializer;
 import org.springframework.data.jpa.repository.config.EnableJpaRepositories;
 import org.springframework.web.servlet.config.annotation.EnableWebMvc;
 
 @SpringBootApplication
 @EnableWebMvc
 @EnableJpaRepositories
-public class TestApplication extends SpringBootServletInitializer {
+public class TestApplication {
 
 	public static void main(String[] args) {
 		SpringApplication.run(TestApplication.class, args);
